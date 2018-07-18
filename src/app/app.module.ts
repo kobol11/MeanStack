c5fd8abb588f7141e4e3b4d268ee2d090b2c94f0
Bolarinwa <komolafe.bolarinwa@gmail.com>
2018-07-18 16:32:41 -0400

    create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { ContentComponent } from './content/content.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { PositionsComponent } from './positions/positions.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    ContentComponent,
+    FooterComponent,
+    HomeComponent,
+    EmployeesComponent,
+    PositionsComponent
   ],
   imports: [
     BrowserModule,
