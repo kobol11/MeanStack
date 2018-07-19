4fbd0c11b66924b714d66ec1c080cb081cdf9749
Bolarinwa <komolafe.bolarinwa@gmail.com>
2018-07-18 20:21:40 -0400

    add getEmployees method to employee service

--- src/app/data/employee.service.ts
+++ src/app/data/employee.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Employee} from './employee';
+import { Observable } from '../../../node_modules/rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
 
-  constructor() { }
+  constructor(private http : HttpClient) { }
+
+  getEmployees() : Observable<Employee[]>{
+    return this.http.get<Employee[]>("/employees");
+  }
 }
