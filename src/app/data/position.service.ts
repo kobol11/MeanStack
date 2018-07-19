59575b217e077ee8652404e2afa4a5783c088fb8
Bolarinwa <komolafe.bolarinwa@gmail.com>
2018-07-18 20:17:48 -0400

    add getPosition method to position service

--- src/app/data/position.service.ts
+++ src/app/data/position.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http'
+import {Position} from './position'
+import { Observable } from '../../../node_modules/rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PositionService {
 
-  constructor() { }
+  constructor(private http : HttpClient) { }
+
+  getPositions() : Observable<Position[]>{
+    return this.http.get<Position[]>('/positions');
+  }
 }
