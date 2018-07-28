1807bf4b0662d11c4ca4016e3dd3f3ea3df848f8
Bolarinwa <komolafe.bolarinwa@gmail.com>
2018-07-28 18:18:18 -0400

    add methods to employee.component.ts

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -1,15 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import {EmployeeRaw} from '../data/employeeRaw';
+import {Position} from '../data/position';
+import {ActivatedRoute} from '@angular/router';
+import {EmployeeService} from '../data/employee.service';
+import {PositionService} from '../data/position.service';
 
 @Component({
   selector: 'app-employee',
   templateUrl: './employee.component.html',
   styleUrls: ['./employee.component.css']
 })
-export class EmployeeComponent implements OnInit {
-
-  constructor() { }
+export class EmployeeComponent implements OnInit, OnDestroy {
+  paramSubscription : any;
+  employeeSubscription : any;
+  getPositionsSubscription : any;
+  saveEmployeeSubscription : any;
+  employee : EmployeeRaw;
+  positions : Position[];
+  successMessage : boolean = false;
+  failMessage : boolean = false;
+  constructor(private empService : EmployeeService, private positionService : PositionService, private route : ActivatedRoute) { }
 
   ngOnInit() {
+    let id;
+    this.paramSubscription =  this.route.params.subscribe(params =>{id = +params['_id']});
+    this.employeeSubscription = this.empService.getEmployee(this.paramSubscription).subscribe(user => this.employee = user[0]);
+    this.getPositionsSubscription = this.positionService.getPositions().subscribe(positions => this.positions = positions);
   }
 
+  onSubmit(){
+    this.saveEmployeeSubscription = this.empService.saveEmployee(this.employee).subscribe((success) =>{
+      this.successMessage = true;
+      setTimeout(() => {
+        this.successMessage = false;
+      }, 2500);
+    }, (fail) =>{ 
+      this.failMessage = true;
+      setTimeout(()=>{this.failMessage = false;}, 2500); 
+      
+    })
+  }
+
+  ngOnDestroy(){
+    if(this.saveEmployeeSubscription){
+      this.saveEmployeeSubscription.unsubscribe();
+    }
+    if(this.paramSubscription){
+      this.paramSubscription.unsubscribe();
+    }
+    if(this.employeeSubscription){
+      this.employeeSubscription.unsubscribe();
+    }
+  if(this.getPositionsSubscription){
+    this.getPositionsSubscription.unsubscribe();
+  }
+  }
 }
