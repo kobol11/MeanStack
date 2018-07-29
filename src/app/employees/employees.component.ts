1f1b9e0c4fac2ccdea8ceb92544af91ef2cddea1
Bolarinwa <komolafe.bolarinwa@gmail.com>
2018-07-29 16:47:35 -0400

    add event handler for filter function

--- src/app/employees/employees.component.ts
+++ src/app/employees/employees.component.ts
@@ -12,18 +12,29 @@ export class EmployeesComponent implements OnInit, OnDestroy {
   employees : Employee[];
   loadingError : boolean = false;
   getEmployeesSub : any;
+  filteredEmployees: Employee[];
   constructor(private service : EmployeeService, private router : Router) { }
 
   ngOnInit() {
     this.getEmployeesSub = this.service.getEmployees().subscribe(users => {
       this.employees = users;
-      console.log(users);
+      this.filteredEmployees = users;
     });
     
   }
 
   routeEmployee(id : string){
     this.router.navigate(['/employee', id]);
+  }
+
+  onEmployeeSearchKeyUp(event: any){
+    this.filteredEmployees = this.employees.filter(employee =>{
+    if(event.target.value){
+      return  employee.FirstName.toUpperCase().indexOf(event.target.value.toUpperCase()) > -1 || employee.LastName.toUpperCase().indexOf(event.target.value.toUpperCase()) > -1 || employee.Position.PositionName.toUpperCase().indexOf(event.target.value.toUpperCase()) > -1;
+    }else{
+      return employee;
+    }
+    });
   }
 
   ngOnDestroy(){
